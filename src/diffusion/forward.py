cc791e1dbda13e18bc5d8f4c1bbb674e7f56d19e
Malk97sc <malaksanchez09@gmail.com>
2025-12-05 18:11:44 -0500

    Forward Process for DDPM

--- src/diffusion/forward.py
+++ src/diffusion/forward.py
@@ -25,3 +25,20 @@ def q_sample(x0, t, alphas_bar):
     sqrt_ab = torch.sqrt(alphas_bar[t])
     sqrt_one_minus_ab = torch.sqrt(1 - alphas_bar[t])
     return sqrt_ab * x0 + sqrt_one_minus_ab * noise
+
+def q_sample_ddpm(x0, t, alphas_bar):
+    """
+    Batch Function to Forward diffusion q(x_t | x_0)
+
+    x0: (B, C, H, W)
+    t: (B,) timesteps
+    alphas_bar
+
+    returns:
+        x_t: (B, C, H, W)
+        noise: (B, C, H, W) to build x_t
+    """
+    noise = torch.randn_like(x0)
+    ab_t = alphas_bar[t].view(-1, 1, 1, 1)  #(B,1,1,1) 
+    x_t = torch.sqrt(ab_t) * x0 + torch.sqrt(1.0 - ab_t) * noise
+    return x_t, noise
